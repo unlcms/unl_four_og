b1bdb0cf12f0615d9725683f1bd3db21e3c7eb1e
Michael Fairchild <mfairchild365@gmail.com>
2013-09-18 11:56:54 -0500

    Display correct site title for pageviews
    Don't check if the context is a node, because it COULD be a viewpage.
    Also remove unneeded code.

--- template.php
+++ template.php
@@ -6,23 +6,12 @@
 function unl_four_og_preprocess_page(&$vars, $hook) {
   if (module_exists('og_context')) {
     // Set site_name to Group's display name.
-    if (!empty($vars['node'])) {
-      $group_context = og_context();
+    $group_context = og_context();
 
-      // Make sure that the current page has a group associated with it.
-      if ($group_context && $group = node_load($group_context['gid'])) {
-        $vars['site_name'] = $group->title;
-      }
+    // Make sure that the current page has a group associated with it.
+    if ($group_context && $group = node_load($group_context['gid'])) {
+      $vars['site_name'] = $group->title;
     }
-//     //if not dealing with a node, Are we still in group context - views?
-//     if(!$vars['og_id'] && $group = og_get_group_context()){
-//         $vars['site_name'] = print_r($vars['site_name'],true);
-//         $vars['og'] =$group->title;
-//         $vars['og_id'] = $group->nid;
-//     }
-//     if ($vars['og_id']) {
-//       $vars['site_name'] = $vars['og'] . ' <span>&nbsp;' . $vars['site_name'] . '</span>';
-//     }
   }
 }
 
@@ -118,8 +107,8 @@ function unl_four_og_breadcrumb($variables) {
   else {
     // Change 'Home' to be $site_name
     array_unshift($variables['breadcrumb'],
-                  str_replace('Home', check_plain(unl_four_get_site_name_abbreviated()),
-                  array_shift($variables['breadcrumb'])));
+      str_replace('Home', check_plain(unl_four_get_site_name_abbreviated()),
+        array_shift($variables['breadcrumb'])));
   }
 
   // Prepend UNL
